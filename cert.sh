a55a3a9f2b80ada7f0c392f12545a15d16d8064a
jean-roch <vlimant@cern.ch>
2014-10-16 12:42:02 +0200

    cert and collect separated scripts

---
+++ cert.sh
@@ -0,0 +1,8 @@
+## setup a cannocial location for a proxy
+export X509_USER_PROXY=$HOME/cert/voms_proxy.cert
+
+## make sure the protection is right 
+chmod 400 $HOME/cert/voms_proxy.cert
+
+## create a long lived proxy in the designated location
+cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 48:00 -pwstdin
--- collect.sh
+++ collect.sh
@@ -1,17 +1,7 @@
 ## this can be a soft link
 cd $HOME/ntprod/
 
-## setup a cannocial location for a proxy
-mkdir $HOME/cert
-export X509_USER_PROXY=$HOME/cert/voms_proxy.cert
-#export X509_USER_KEY=$HOME/cert/voms_proxy.cert
-#export X509_USER_CERT=$HOME/cert/voms_proxy.cert
-
-## make sure the protection is right 
-chmod 400 $HOME/cert/voms_proxy.cert
-
-## create a long lived proxy in the designated location
-cat $HOME/private/$USER.txt | voms-proxy-init -voms cms --valid 48:00 -pwstdin
+source cert.sh
 
 ## run the task collection
-./production.py --do collect
+./production.py --do collect $1

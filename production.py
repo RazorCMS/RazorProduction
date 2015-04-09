691c8e2872030b211138486ffa4215c2b2df974b
jean-roch <vlimant@cern.ch>
2015-04-09 23:45:48 +0200

    double confirm to crab kill

--- production.py
+++ production.py
@@ -651,12 +651,18 @@ if options.do in ['list','create','submit','reset','collect','acquire']:
             ## do the hard reset of a task regardless of what is the status in crab
             K=raw_input("Go with killing task %s ? Y/N :"%( r['taskname']))
             if K.lower() in ['y','yes']:
-                dead = crabKill( r['taskname'], r['assignee'])
-                print "Crab kill output",dead
-            if not dead:
-                print "Crab kill failed. Please retry to reset again"
+                K=raw_input("Confirm that you want to kill all for task %s ? Y/N :"%( r['taskname']))
+                if K.lower() in ['y','yes']:
+                    dead = crabKill( r['taskname'], r['assignee'])
+                    print "Crab kill output",dead
+                    if not dead:
+                        print "Crab kill failed. Please retry to reset again"
+                        continue
+                else:
+                    continue
+            else:
                 continue
-                
+
             K=raw_input("Go with ressetting %s @ %s \n ? Y/N :"%(r['id'], r['assignee']))
             if K.lower() in ['y','yes']:
                 print "ressetting",r['id']
